43e3b5122a50fe33fb9e021a7b660ba368fc6aa4
LaureanoDev <laureanoperezlindodev@hotmail.com>
2023-04-05 15:47:59 -0300

    notifications post added

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -2,7 +2,8 @@
 
 import { useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import toast from "react-hot-toast";
 
 export default function AddPost() {
   const [title, setTitle] = useState("");
@@ -10,14 +11,16 @@ export default function AddPost() {
 
   //Generate a post
   const { mutate } = useMutation(
-    async (title: string) =>
-      await axios.post("/api/posts/addPost", { title }),
+    async (title: string) => await axios.post("/api/posts/addPost", { title }),
     {
       onError: (error) => {
-        console.log("error: ", error);
+        if (error instanceof AxiosError) {
+          toast.error(error?.response?.data.message);
+        }
+        setIsDisabled(false)
       },
       onSuccess: (data) => {
-        console.log(data);
+        toast.success("Post has been made 🔥")
         setTitle("");
         setIsDisabled(false);
       },
